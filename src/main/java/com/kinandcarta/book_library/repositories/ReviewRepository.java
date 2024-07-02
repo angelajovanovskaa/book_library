23b2eb2d227b8ed2655ba1eb390ba283eed58c42
mbojkovski <mbojkovski@melontech.com>
2024-07-02 15:01:36 +0200

    Changed the JpaRepository attributes from Long to UUID.

--- src/main/java/com/kinandcarta/book_library/repositories/ReviewRepository.java
+++ src/main/java/com/kinandcarta/book_library/repositories/ReviewRepository.java
@@ -3,5 +3,7 @@ package com.kinandcarta.book_library.repositories;
 import com.kinandcarta.book_library.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+import java.util.UUID;
+
+public interface ReviewRepository extends JpaRepository<Review, UUID> {
 }
