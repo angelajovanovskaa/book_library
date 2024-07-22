485fdaa85ac7203b3568b296e75f5174c9e3c646
ljupcho <ljupcoristov02@gmail.com>
2024-07-22 12:14:48 +0200

    Adding Javadoc to method getAverageRatingOnBook in CalculateAverageRatingOnBookImpl.java

--- src/main/java/com/kinandcarta/book_library/services/impl/CalculateAverageRatingOnBookImpl.java
+++ src/main/java/com/kinandcarta/book_library/services/impl/CalculateAverageRatingOnBookImpl.java
@@ -1,16 +1,25 @@
 package com.kinandcarta.book_library.services.impl;
 
+import com.kinandcarta.book_library.entities.Book;
+import com.kinandcarta.book_library.entities.Review;
 import com.kinandcarta.book_library.services.CalculateAverageRatingOnBook;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 /**
- * Performs calculation to provide a average rating (of type double) for given Book object.
+ * Performs calculation to provide an average rating (of type double) for given {@link Book} object.
  */
 @Component
 public class CalculateAverageRatingOnBookImpl implements CalculateAverageRatingOnBook {
-    
+
+    /**
+     * Calculates average from List of Integers ({@link Review} ratings)
+     * <hr>
+     *
+     * @param reviews Type: List of Integers
+     * @return double
+     */
     public double getAverageRatingOnBook(List<Integer> reviews) {
 
         double sumReviews = 0;
