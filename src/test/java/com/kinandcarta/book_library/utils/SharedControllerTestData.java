0489b9425859db8f6c05643db721f757408684ee
ljupcho <ljupcoristov02@gmail.com>
2024-08-15 14:17:27 +0200

    Created new SharedControllerTestData class.

---
+++ src/test/java/com/kinandcarta/book_library/utils/SharedControllerTestData.java
@@ -0,0 +1,9 @@
+package com.kinandcarta.book_library.utils;
+
+public class SharedControllerTestData {
+    public static final String OFFICE_PARAM = "officeName";
+    public static final String BOOK_TITLE_PARAM = "titleSearchTerm";
+    public static final String USER_ID_PARAM = "userId";
+    public static final String PAGE_SIZE_PARAM = "pageSize";
+    public static final String CONTENT_KEY_VALUE = "content";
+}
