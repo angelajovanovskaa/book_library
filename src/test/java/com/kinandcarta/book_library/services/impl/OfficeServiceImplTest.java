4984f25b4812c86cb452eb192d6a028d5f5fd3a7
ljupcho <ljupcoristov02@gmail.com>
2024-08-15 13:34:49 +0200

    Changed imports of test data in Office services tests, renaming of methods, adapting services tests to use test utility data.

--- src/test/java/com/kinandcarta/book_library/services/impl/OfficeServiceImplTest.java
+++ src/test/java/com/kinandcarta/book_library/services/impl/OfficeServiceImplTest.java
@@ -3,6 +3,7 @@ package com.kinandcarta.book_library.services.impl;
 import com.kinandcarta.book_library.dtos.OfficeResponseDTO;
 import com.kinandcarta.book_library.entities.Office;
 import com.kinandcarta.book_library.repositories.OfficeRepository;
+import com.kinandcarta.book_library.utils.SharedTestData;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +16,7 @@ import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class OfficeServiceImplTest {
+
     @Mock
     private OfficeRepository officeRepository;
 
@@ -22,7 +24,7 @@ class OfficeServiceImplTest {
     private OfficeServiceImpl officeService;
 
     @Test
-    void getAllOffices_theListHasAtLeastOne_returnsListOfOfficeResponseDTO() {
+    void getAllOffices_atLeastOneOfficeExists_returnsListOfOfficeResponseDTO() {
         // given
         List<Office> offices = getOffices();
         List<OfficeResponseDTO> officeResponseDTOs = getOfficeResponseDTOs();
@@ -30,23 +32,17 @@ class OfficeServiceImplTest {
         given(officeRepository.findAll()).willReturn(offices);
 
         // when
-        List<OfficeResponseDTO> result = officeService.getAllOffices();
+        List<OfficeResponseDTO> actualResult = officeService.getAllOffices();
 
         //then
-        assertThat(result).isEqualTo(officeResponseDTOs);
+        assertThat(actualResult).isEqualTo(officeResponseDTOs);
     }
 
     private List<Office> getOffices() {
-        Office office1 = new Office("Skopje");
-        Office office2 = new Office("Sofija");
-
-        return List.of(office1, office2);
+        return List.of(SharedTestData.SKOPJE_OFFICE, SharedTestData.SOFIJA_OFFICE);
     }
 
     private List<OfficeResponseDTO> getOfficeResponseDTOs() {
-        OfficeResponseDTO office1 = new OfficeResponseDTO("Skopje");
-        OfficeResponseDTO office2 = new OfficeResponseDTO("Sofija");
-
-        return List.of(office1, office2);
+        return List.of(SharedTestData.SKOPJE_OFFICE_DTO, SharedTestData.SOFIJA_OFFICE_DTO);
     }
 }
