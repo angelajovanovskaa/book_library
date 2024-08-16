578bd2102543c380856e92d9076337e6e94e5b48
mbojkovski <mbojkovski@melontech.com>
2024-08-16 13:45:38 +0200

    Added new validations in DTO.

--- src/main/java/com/kinandcarta/book_library/dtos/UserLoginRequestDTO.java
+++ src/main/java/com/kinandcarta/book_library/dtos/UserLoginRequestDTO.java
@@ -1,9 +1,10 @@
 package com.kinandcarta.book_library.dtos;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserLoginRequestDTO(
-        @NotBlank
+        @Email(regexp = "\\w+\\.\\w+@(kinandcarta|valtech)\\.com")
         String userEmail,
         @NotBlank
         String userPassword
