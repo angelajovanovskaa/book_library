8837aff3807d4698d4995faa34b1956fce1252b1
ljupcho <ljupcoristov02@gmail.com>
2024-08-16 15:10:51 +0200

    Changes in ReviewQueryController and ReviewQueryControllerTest - renaming of methods in ReviewQueryController - implementing new ReviewQueryControllerTest

--- src/main/java/com/kinandcarta/book_library/controllers/ReviewQueryController.java
+++ src/main/java/com/kinandcarta/book_library/controllers/ReviewQueryController.java
@@ -1,13 +1,18 @@
 package com.kinandcarta.book_library.controllers;
 
-
 import com.kinandcarta.book_library.dtos.ReviewResponseDTO;
 import com.kinandcarta.book_library.services.ReviewQueryService;
-import java.util.List;
-import java.util.UUID;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,25 +22,23 @@ public class ReviewQueryController {
     private final ReviewQueryService service;
 
     @GetMapping("")
-    public ResponseEntity<List<ReviewResponseDTO>> findAllReviewsForBook(@RequestParam String officeName,
-                                                                         @RequestParam String bookISBN) {
+    public ResponseEntity<List<ReviewResponseDTO>> getAllReviewsForBook(@RequestParam @NotBlank String officeName,
+                                                                        @RequestParam @NotBlank String bookISBN) {
         List<ReviewResponseDTO> result = service.getAllReviewsByBookIsbnAndByOfficeName(bookISBN, officeName);
-
 
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/{reviewId}")
-    public ResponseEntity<ReviewResponseDTO> findReviewById(@PathVariable String reviewId) {
-        UUID uuid = UUID.fromString(reviewId);
-        ReviewResponseDTO result = service.getReviewById(uuid);
+    public ResponseEntity<ReviewResponseDTO> getReviewById(@PathVariable @NotBlank String reviewId) {
+        ReviewResponseDTO result = service.getReviewById(UUID.fromString(reviewId));
 
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/top-reviews")
-    public ResponseEntity<List<ReviewResponseDTO>> getTopReviewsForBookView(@RequestParam String officeName,
-                                                                            @RequestParam String bookISBN) {
+    public ResponseEntity<List<ReviewResponseDTO>> getTopReviewsForBookView(@RequestParam @NotBlank String officeName,
+                                                                            @RequestParam @NotBlank String bookISBN) {
         List<ReviewResponseDTO> result = service.getTopReviewsForDisplayInBookView(bookISBN, officeName);
 
         return ResponseEntity.ok(result);
