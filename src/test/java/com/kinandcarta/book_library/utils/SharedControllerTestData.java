8452fd67c326f30300a6db64ef56e88edd788a56
ljupcho <ljupcoristov02@gmail.com>
2024-08-19 11:35:21 +0200

    Removed helper methods from SharedControllerTestData

--- src/test/java/com/kinandcarta/book_library/utils/SharedControllerTestData.java
+++ src/test/java/com/kinandcarta/book_library/utils/SharedControllerTestData.java
@@ -1,8 +1,6 @@
 package com.kinandcarta.book_library.utils;
 
 import lombok.experimental.UtilityClass;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 @UtilityClass
 public class SharedControllerTestData {
@@ -12,26 +10,4 @@ public class SharedControllerTestData {
     public static final String USER_ID_PARAM = "userId";
     public static final String PAGE_SIZE_PARAM = "pageSize";
     public static final String CONTENT_KEY_VALUE = "content";
-
-    public static MultiValueMap<String, String> getMapWithOfficeParamAndBookISBNParam(){
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add(OFFICE_PARAM, SharedServiceTestData.SKOPJE_OFFICE_NAME);
-        map.add(BOOK_ISBN_PARAM, BookTestData.BOOK_ISBN);
-
-        return map;
-    }
-
-    public static MultiValueMap<String, String> getMapWithOfficeParam(){
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add(OFFICE_PARAM, SharedServiceTestData.SKOPJE_OFFICE_NAME);
-
-        return map;
-    }
-
-    public static MultiValueMap<String, String> getMapWithBookISBNParam(){
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add(BOOK_ISBN_PARAM, BookTestData.BOOK_ISBN);
-
-        return map;
-    }
 }
