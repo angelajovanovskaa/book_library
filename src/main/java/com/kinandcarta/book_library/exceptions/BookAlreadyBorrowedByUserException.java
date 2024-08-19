42e614b510c1a31622d77282ef82561b686c860c
mbojkovski <mbojkovski@melontech.com>
2024-08-19 10:43:58 +0200

    Changed the exceptions inheritance from CustomBadRequest to CustomUnprocessableEntity exception.

--- src/main/java/com/kinandcarta/book_library/exceptions/BookAlreadyBorrowedByUserException.java
+++ src/main/java/com/kinandcarta/book_library/exceptions/BookAlreadyBorrowedByUserException.java
@@ -1,6 +1,6 @@
 package com.kinandcarta.book_library.exceptions;
 
-public class BookAlreadyBorrowedByUserException extends CustomBadRequestException {
+public class BookAlreadyBorrowedByUserException extends CustomUnprocessableEntityException {
     public BookAlreadyBorrowedByUserException(String isbn) {
         super("The user already has an instance borrowed from the book with isbn: " + isbn);
     }
