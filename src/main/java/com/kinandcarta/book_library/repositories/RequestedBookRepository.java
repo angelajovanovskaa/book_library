b54d22dfa64838ba1e68a64eb2d381f6c444038c
ljupcho <ljupcoristov02@gmail.com>
2024-08-23 14:15:30 +0200

    - Modifying findAllByOfficeName query and order

--- src/main/java/com/kinandcarta/book_library/repositories/RequestedBookRepository.java
+++ src/main/java/com/kinandcarta/book_library/repositories/RequestedBookRepository.java
@@ -13,7 +13,13 @@ public interface RequestedBookRepository extends JpaRepository<RequestedBook, UU
 
     @Query("select rb from RequestedBook rb " +
             "join fetch rb.book b " +
-            "where b.office.name = :officeName")
+            "where b.office.name = :officeName " +
+            "order by case b.bookStatus " +
+            "    when com.kinandcarta.book_library.enums.BookStatus.REQUESTED then 1 " +
+            "    when com.kinandcarta.book_library.enums.BookStatus.PENDING_PURCHASE then 2 " +
+            "    when com.kinandcarta.book_library.enums.BookStatus.REJECTED then 3 " +
+            "end, " +
+            "rb.requestedDate asc")
     List<RequestedBook> findAllByOfficeName(String officeName);
 
     @Query("select rb from RequestedBook rb " +
