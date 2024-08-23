4464b760990a3eaf872c50f20eefa172bc7d0d5b
LozanovskaS <dlozanovska28@gmail.com>
2024-08-23 12:36:27 +0200

    Deleted the class

--- src/main/java/com/kinandcarta/book_library/exceptions/DuplicateReviewException.java
+++
@@ -1,7 +0,0 @@
-package com.kinandcarta.book_library.exceptions;
-
-public class DuplicateReviewException extends CustomUnprocessableEntityException{
-    public DuplicateReviewException() {
-        super("The user has already left a review for this book.");
-    }
-}
