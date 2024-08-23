d7745683c1e9a0e4ab3bbcb0ab40e405e001e0d7
ljupcho <ljupcoristov02@gmail.com>
2024-08-23 15:25:50 +0200

    - Changed message in exception

--- src/main/java/com/kinandcarta/book_library/exceptions/BookStatusChangeBadRequestException.java
+++ src/main/java/com/kinandcarta/book_library/exceptions/BookStatusChangeBadRequestException.java
@@ -2,6 +2,6 @@ package com.kinandcarta.book_library.exceptions;
 
 public class BookStatusChangeBadRequestException extends CustomBadRequestException {
     public BookStatusChangeBadRequestException() {
-        super("Book status change failed, invalid new status");
+        super("Book status change failed, cannot use IN_STOCK as new book status");
     }
 }
