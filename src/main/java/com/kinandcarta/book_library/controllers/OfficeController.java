2cce2db0c618ce9e377382b07f74a4cf5e2991f7
LozanovskaS <dlozanovska28@gmail.com>
2024-08-26 14:15:56 +0200

    Added Validated annotation

--- src/main/java/com/kinandcarta/book_library/controllers/OfficeController.java
+++ src/main/java/com/kinandcarta/book_library/controllers/OfficeController.java
@@ -4,6 +4,7 @@ import com.kinandcarta.book_library.dtos.OfficeResponseDTO;
 import com.kinandcarta.book_library.services.OfficeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/offices")
+@Validated
 public class OfficeController {
 
     private final OfficeService officeService;
