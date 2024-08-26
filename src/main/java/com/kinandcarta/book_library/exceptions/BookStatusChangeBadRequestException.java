9de46cf3154d4dd8c75aa448da934aa2d71c7ac7
ljupcho <ljupcoristov02@gmail.com>
2024-08-26 10:43:54 +0200

    - Deleted class BookStatusChangeBadRequestException.java

--- src/main/java/com/kinandcarta/book_library/exceptions/BookStatusChangeBadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.kinandcarta.book_library.exceptions;
-
-public class BookStatusChangeBadRequestException extends CustomBadRequestException {
-    public BookStatusChangeBadRequestException() {
-        super("Book status change failed, cannot use IN_STOCK as new book status");
-    }
-}
