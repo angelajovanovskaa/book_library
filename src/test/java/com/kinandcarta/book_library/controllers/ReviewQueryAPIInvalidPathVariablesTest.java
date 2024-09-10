658561842e9a6b6f9f2c8d144b1e6ab21da04d55
mbojkovski <mbojkovski@melontech.com>
2024-09-10 12:51:48 +0200

    Fixed pipeline.

--- src/test/java/com/kinandcarta/book_library/controllers/ReviewQueryAPIInvalidPathVariablesTest.java
+++ src/test/java/com/kinandcarta/book_library/controllers/ReviewQueryAPIInvalidPathVariablesTest.java
@@ -1,13 +1,16 @@
 package com.kinandcarta.book_library.controllers;
 
+import com.kinandcarta.book_library.config.JwtService;
 import com.kinandcarta.book_library.services.ReviewManagementService;
 import com.kinandcarta.book_library.services.ReviewQueryService;
+import com.kinandcarta.book_library.services.impl.UserQueryServiceImpl;
 import com.kinandcarta.book_library.utils.ErrorMessages;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ReviewController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class ReviewQueryAPIInvalidPathVariablesTest {
     private static final String GET_REVIEW_PATH = "/reviews/";
     private static final String DETAIL_JSON_PATH = "$.detail";
@@ -26,6 +30,12 @@ class ReviewQueryAPIInvalidPathVariablesTest {
 
     @MockBean
     private ReviewManagementService reviewManagementService;
+
+    @MockBean
+    private JwtService jwtService;
+
+    @MockBean
+    private UserQueryServiceImpl userQueryService;
 
     @Autowired
     private MockMvc mockMvc;
